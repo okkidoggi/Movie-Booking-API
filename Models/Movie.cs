a07f4b306565867a6ea95af2e36d8d4cf97b3cf6
okkidoggi <tengweiyuen@hotmail.com>
2022-03-11 15:10:52 +0800

    Add project files.

---
+++ Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movie_Booking_API.Models
+{
+    public class Movie
+    {
+        public long Id { get; set; }
+        public string MovieName { get; set; }
+        public string DateTime { get; set; }
+        public string Venue { get; set; }
+        public string CinemaRoom { get; set; }
+        public int NumberOfTickets { get; set; }
+        public double TicketPrice { get; set; }
+
+    }
+}
---
+++ Models/MovieContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movie_Booking_API.Models
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
